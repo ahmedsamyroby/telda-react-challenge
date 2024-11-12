c2315406d45b3bbabe5cd191b4af2a6e5c496121
Ahmed Samy <ahmedsamyroby@gmail.com>
2024-11-13 00:55:36 +0200

    feat: add skeleton for movie card

--- src/pages/MoviesList/components/MovieCard/index.tsx
+++ src/pages/MoviesList/components/MovieCard/index.tsx
@@ -23,13 +23,33 @@ export default function MovieCard({
           <span className="font-semibold text-xs sm:text-sm text-secondary-typography">
             {releaseDate}
           </span>
-          <h3 className="text-md lg:text-lg text-primary-typography">
+          <h3
+            className="text-md lg:text-lg text-primary-typography max-w-48 truncate"
+            title={title}
+          >
             {title}
           </h3>
         </div>
         <button className="bg-accent border-2 border-accent p-1 rounded-md text-sm text-primary-typography hover:bg-transparent hover:border-accent hover:text-accent transition-colors">
           <Info />
         </button>
+      </div>
+    </article>
+  );
+}
+
+export function MovieCardSkeleton() {
+  return (
+    <article className="bg-secondary shadow rounded-md overflow-hidden animate-pulse">
+      <div className="aspect-[27/40] bg-gray-300">
+        <div className="w-full h-full object-cover bg-gray-300" />
+      </div>
+      <div className="py-2 px-4 flex justify-between items-center">
+        <div>
+          <div className="h-4 w-16 bg-gray-300 rounded-sm mb-3" />
+          <div className="h-6 w-36 bg-gray-300 rounded-sm" />
+        </div>
+        <div className="w-9 h-9 bg-gray-300 rounded-md" />
       </div>
     </article>
   );
