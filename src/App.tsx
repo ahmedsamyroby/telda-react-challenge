122d40d773392938a02f27fe1ea041a41eda8218
Ahmed Samy <ahmedsamyroby@gmail.com>
2024-11-13 02:01:47 +0200

    chore: add react-query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ function App() {
           <Outlet />
         </div>
       </div>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
