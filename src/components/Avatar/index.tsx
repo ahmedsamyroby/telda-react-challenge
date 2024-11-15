348e37abf14d6ac5f9477cc05097d9bc56a98511
Ahmed Samy <ahmedsamyroby@gmail.com>
2024-11-15 20:59:50 +0200

    feat: refactor Avatar component to handle image loading errors with fallback display

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,19 +1,56 @@
 import clsx from "clsx";
-import { getInitials } from "../../utils";
+import { useState } from "react";
 
 type AvatarProps = React.AllHTMLAttributes<HTMLDivElement> & {
-  name: string;
+  image: AvatarImageProps;
+  fallback: AvatarFallbackProps;
 };
 
-export default function Avatar({ name, className }: AvatarProps) {
+export default function Avatar({ image, fallback, ...props }: AvatarProps) {
+  const [isError, setIsError] = useState(false);
+  return (
+    <div {...props}>
+      {!isError ? (
+        <AvatarImage onError={() => setIsError(true)} {...image} />
+      ) : (
+        <AvatarFallback {...fallback} />
+      )}
+    </div>
+  );
+}
+
+type AvatarImageProps = React.AllHTMLAttributes<HTMLImageElement>;
+
+function AvatarImage({
+  src,
+  alt = "Avatar",
+  className,
+  ...props
+}: AvatarImageProps) {
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className={clsx("w-full h-full object-cover", className)}
+      {...props}
+    />
+  );
+}
+
+type AvatarFallbackProps = {
+  content: React.ReactNode;
+  className?: string;
+};
+
+function AvatarFallback({ content = "NA", className }: AvatarFallbackProps) {
   return (
     <div
       className={clsx(
-        "w-full h-full font-bold text-5xl flex items-center justify-center",
+        "font-bold flex items-center justify-center h-full w-full",
         className
       )}
     >
-      {getInitials(name)}
+      {content}
     </div>
   );
 }
--- src/pages/MovieDetails/components/CastCard/index.tsx
+++ src/pages/MovieDetails/components/CastCard/index.tsx
@@ -1,5 +1,6 @@
 import Avatar from "../../../../components/Avatar";
 import { POSTER_BASE_URL } from "../../../../constants";
+import { getInitials } from "../../../../utils";
 
 type CastCardProps = {
   image?: string | null;
@@ -10,17 +11,17 @@ type CastCardProps = {
 export default function CastCard({ image, name, character }: CastCardProps) {
   return (
     <div className="flex flex-col items-center w-40 min-w-40 text-center">
-      <div className="w-36 h-36 rounded-md mb-4 overflow-hidden">
-        {!image ? (
-          <Avatar className="bg-primary text-accent" name={name} />
-        ) : (
-          <img
-            alt={name}
-            src={POSTER_BASE_URL + image}
-            className="h-full w-full object-cover"
-          />
-        )}
-      </div>
+      <Avatar
+        className="w-36 h-36 rounded-md mb-4 overflow-hidden"
+        image={{
+          src: POSTER_BASE_URL + (image ?? ""),
+          alt: name,
+        }}
+        fallback={{
+          content: getInitials(name),
+          className: "text-5xl bg-primary text-accent",
+        }}
+      />
       <p className="text-lg leading-5 text-primary-typography mb-2">{name}</p>
       <p className="text-sm text-secondary-typography font-semibold">
         {character}
