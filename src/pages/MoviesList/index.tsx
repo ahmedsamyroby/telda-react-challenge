ae4705ea87f2ea1026a14fa0a256cdbcdb111b0c
Ahmed Samy <ahmedsamyroby@gmail.com>
2024-11-16 13:14:39 +0200

    feat: add SearchBar component for movie search functionality and integrate with MoviesList

--- src/pages/MoviesList/index.tsx
+++ src/pages/MoviesList/index.tsx
@@ -1,31 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Pagination from "../../components/Pagination";
 import { IMAGE_BASE_URL } from "../../constants";
 import useGetMoviesListQuery from "../../hooks/apis/useGetMoviesListQuery";
 import MovieCard, { MovieCardSkeleton } from "./components/MovieCard";
 import { CircleAlert } from "lucide-react";
-import { useSearchParams } from "react-router-dom";
+import SearchBar from "./components/SearchBar";
 
 const PAGE_SIZE = 20;
 const MAX_PAGES = 500; // TheMovieDB API only allows up to 500 pages
 
 export default function MoviesList() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const currentPage = Number(searchParams.get("page")) || 1;
+  const [movieName, setMovieName] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
 
   const {
     data: moviesList,
     isLoading,
     isError,
     error,
-  } = useGetMoviesListQuery(currentPage);
+  } = useGetMoviesListQuery(currentPage, movieName);
 
-  const handlePageChange = (page: number) =>
-    setSearchParams({ page: page.toString() });
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
 
   useEffect(() => {
-    if (!searchParams.get("page")) setSearchParams({ page: "1" });
-  }, [searchParams, setSearchParams]);
+    setCurrentPage(1);
+  }, [movieName]);
 
   if (isError)
     return (
@@ -36,28 +37,38 @@ export default function MoviesList() {
     );
 
   return (
-    <div className="flex flex-col gap-12 w-full">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
-        {isLoading
-          ? Array.from({ length: PAGE_SIZE }).map((_, idx) => (
-              <MovieCardSkeleton key={idx} />
-            ))
-          : (moviesList?.results ?? []).map((movie) => (
-              <MovieCard
-                key={movie.id}
-                id={movie.id}
-                image={IMAGE_BASE_URL + movie.poster_path}
-                title={movie.title}
-                releaseDate={movie.release_date}
-              />
-            ))}
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-full flex justify-center items-center py-3 bg-secondary">
+        <SearchBar setMovieName={setMovieName} />
       </div>
-      <div className="flex items-center justify-center">
-        <Pagination
-          currentPage={currentPage}
-          totalPages={Math.min(moviesList?.total_pages || 0, MAX_PAGES)}
-          onPageChange={handlePageChange}
-        />
+      <div className="max-w-[100rem] w-full">
+        <h1 className="text-5xl text-accent font-bold text-center mt-8">
+          {movieName ? `Search Results for "${movieName}"` : "Movies"}
+        </h1>
+        <div className="flex flex-col gap-12 w-full p-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
+            {isLoading
+              ? Array.from({ length: PAGE_SIZE }).map((_, idx) => (
+                  <MovieCardSkeleton key={idx} />
+                ))
+              : (moviesList?.results ?? []).map((movie) => (
+                  <MovieCard
+                    key={movie.id}
+                    id={movie.id}
+                    image={IMAGE_BASE_URL + movie.poster_path}
+                    title={movie.title}
+                    releaseDate={movie.release_date}
+                  />
+                ))}
+          </div>
+          <div className="flex items-center justify-center">
+            <Pagination
+              currentPage={currentPage}
+              totalPages={Math.min(moviesList?.total_pages || 0, MAX_PAGES)}
+              onPageChange={handlePageChange}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
