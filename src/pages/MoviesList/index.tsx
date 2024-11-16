510b7571b2f714b532d22052954ae3a4b8f7aa12
Ahmed Samy <ahmedsamyroby@gmail.com>
2024-11-16 13:48:25 +0200

    feat: add no results message in MoviesList when no movies are found

--- src/pages/MoviesList/index.tsx
+++ src/pages/MoviesList/index.tsx
@@ -41,35 +41,41 @@ export default function MoviesList() {
       <div className="w-full flex justify-center items-center py-3 px-10 bg-secondary">
         <SearchBar setMovieName={setMovieName} />
       </div>
-      <div className="max-w-[100rem] w-full">
-        <h1 className="text-3xl sm:text-4xl md:text-5xl text-accent font-bold text-center mt-8 p-2">
-          {movieName ? `Search Results for "${movieName}"` : "Movies"}
-        </h1>
-        <div className="flex flex-col gap-12 w-full p-8">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
-            {isLoading
-              ? Array.from({ length: PAGE_SIZE }).map((_, idx) => (
-                  <MovieCardSkeleton key={idx} />
-                ))
-              : (moviesList?.results ?? []).map((movie) => (
-                  <MovieCard
-                    key={movie.id}
-                    id={movie.id}
-                    image={IMAGE_BASE_URL + movie.poster_path}
-                    title={movie.title}
-                    releaseDate={movie.release_date}
-                  />
-                ))}
-          </div>
-          <div className="flex items-center justify-center">
-            <Pagination
-              currentPage={currentPage}
-              totalPages={Math.min(moviesList?.total_pages || 0, MAX_PAGES)}
-              onPageChange={handlePageChange}
-            />
+      {moviesList?.results.length === 0 ? (
+        <div className="py-20 px-6 text-accent font-bold text-2xl lg:text-4xl flex flex-col justify-center items-center gap-2">
+          Cant find any movies with the name "{movieName}"
+        </div>
+      ) : (
+        <div className="max-w-[100rem] w-full">
+          <h1 className="text-3xl sm:text-4xl md:text-5xl text-accent font-bold text-center mt-8 p-2">
+            {movieName ? `Search Results for "${movieName}"` : "Movies"}
+          </h1>
+          <div className="flex flex-col gap-12 w-full p-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
+              {isLoading
+                ? Array.from({ length: PAGE_SIZE }).map((_, idx) => (
+                    <MovieCardSkeleton key={idx} />
+                  ))
+                : (moviesList?.results ?? []).map((movie) => (
+                    <MovieCard
+                      key={movie.id}
+                      id={movie.id}
+                      image={IMAGE_BASE_URL + movie.poster_path}
+                      title={movie.title}
+                      releaseDate={movie.release_date}
+                    />
+                  ))}
+            </div>
+            <div className="flex items-center justify-center">
+              <Pagination
+                currentPage={currentPage}
+                totalPages={Math.min(moviesList?.total_pages || 0, MAX_PAGES)}
+                onPageChange={handlePageChange}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
