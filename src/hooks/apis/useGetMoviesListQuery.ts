18ace2c6f66cb0fabb9607d61025ce5adafbef80
Ahmed Samy <ahmedsamyroby@gmail.com>
2024-11-16 14:31:11 +0200

    refactor: type the queries correctly

--- src/hooks/apis/useGetMoviesListQuery.ts
+++ src/hooks/apis/useGetMoviesListQuery.ts
@@ -1,6 +1,6 @@
 import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import axiosInstance from "../../services/AxiosService";
-import { AxiosResponse } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
 import TheMovieDBPaginatedResponse from "../../types/TheMovieDBPaginatedResponse";
 import MovieSummary from "../../types/MovieSummary";
 
@@ -8,27 +8,27 @@ type MovieListResponse = Promise<
   AxiosResponse<TheMovieDBPaginatedResponse<MovieSummary>>
 >;
 
-function getMoviesList({
-  queryKey,
-}: {
-  queryKey: (string | number)[];
-}): MovieListResponse {
+function getMoviesList(page: number, movieName?: string): MovieListResponse {
   // themoviedb search endpoint does not support empty queries
   // and the discover endpoint does not support queries
   // so we need to determine which endpoint to use based on the query
-  const endpoint = queryKey[2] ? "/search/movie" : "/discover/movie";
+  const endpoint = movieName ? "/search/movie" : "/discover/movie";
   return axiosInstance.get(endpoint, {
     params: {
-      page: queryKey[1],
-      query: queryKey[2],
+      page,
+      query: movieName,
     },
   });
 }
 
 export default function useGetMoviesListQuery(page: number, movieName: string) {
-  return useQuery({
+  return useQuery<
+    AxiosResponse<TheMovieDBPaginatedResponse<MovieSummary>>,
+    AxiosError,
+    TheMovieDBPaginatedResponse<MovieSummary>
+  >({
     queryKey: ["movies-list", page, movieName],
-    queryFn: getMoviesList,
+    queryFn: () => getMoviesList(page, movieName),
     select: (data) => data.data,
     staleTime: 60 * 1000, // make the movies list stale for 60 seconds
     placeholderData: (previousData) => keepPreviousData(previousData),
